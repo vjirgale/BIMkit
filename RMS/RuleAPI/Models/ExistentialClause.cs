9483f7e83493bc11d414ea51812baa54c735cd09
Christoph Sydora <csydora@ualberta.ca>
2021-05-27 13:37:50 -0600

    Fixed spelling error

--- RMS/RuleAPI/Models/ExistentialClause.cs
+++ RMS/RuleAPI/Models/ExistentialClause.cs
@@ -7,23 +7,23 @@ namespace RuleAPI.Models
     public class ExistentialClause
     {
         [JsonConverter(typeof(StringEnumConverter))]
-        public OccurenceRule OccurenceRule { get; set; }
+        public OccurrenceRule OccurrenceRule { get; set; }
         public Characteristic Characteristic { get; set; }
 
-        public ExistentialClause(OccurenceRule occuranceRule, Characteristic characteristic)
+        public ExistentialClause(OccurrenceRule occurrenceRule, Characteristic characteristic)
         {
-            OccurenceRule = occuranceRule;
+            OccurrenceRule = occurrenceRule;
             Characteristic = characteristic;
         }
 
         public string String(string itemName)
         {
-            return OccurenceRule + " " + itemName + " = " + Characteristic.String();
+            return OccurrenceRule + " " + itemName + " = " + Characteristic.String();
         }
 
         public ExistentialClause Copy()
         {
-            return new ExistentialClause(this.OccurenceRule, this.Characteristic.Copy());
+            return new ExistentialClause(this.OccurrenceRule, this.Characteristic.Copy());
         }
     }
 }
